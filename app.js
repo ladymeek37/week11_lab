6a7ca25a3c95fce4ca15543aed079b88a904d084
Lady Meek <ladyjmeek@gmail.com>
2023-01-13 11:52:30 -0800

    finished problem 7

--- app.js
+++ app.js
@@ -214,6 +214,11 @@ console.log(foodNames)
 //7. Create a function that will append the cuisine type to the start of the dish's name. Ie, "Italian Pizza"
 //Map
 
+let cuisineAndFood = dishes.map(function(el){
+  return el.cuisine + " " + el.name
+});
+console.log(cuisineAndFood);
+
 //8. Create a function that will append the cuisine type to the start of the dish's name. Then, return only the Vegetarian dish objects. 
 //This function should return objects 11-13 with their names changed to "Vegetarian Lasagna", "Vegetarian Falafel", "Vegetarian Chili"
 //Map, Filter
